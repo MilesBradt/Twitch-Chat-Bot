f24438c53f9aa5f821ca90d3da5284e606541888
MilesBradt <smbradtmichael@gmail.com>
2019-07-27 16:58:15 -0700

    add example for hidden config file

---
+++ config-example.js
@@ -0,0 +1,26 @@
+const config = {
+    O_AUTH: "Twitch o_auth goes here",
+    CLIENT_ID: "Twitch API ID goes here",
+    channels: [
+        "user1",
+        "user2",
+        "user3"
+    ],
+    channelsWithBIGFROG: [
+        "user1 ",
+        "user2",
+        "user3"
+    ],
+    channelsNoEmotes: [
+        "user1",
+        "user2",
+        "user3"
+    ],
+    channelUniqueEmotes: [
+        "user1",
+        "user2",
+        "user3"
+    ]
+}
+
+module.exports = config
