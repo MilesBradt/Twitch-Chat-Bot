d17c2c77c3a8240cad39f5e8871c2621a4b0c22b
Miles Bradt <smbradtmichael@gmail.com>
2020-09-19 22:47:26 -0700

    fix async issues for checking online

--- functions/chatCommands.js
+++ functions/chatCommands.js
@@ -1,33 +1,18 @@
+const { client } = require('tmi.js');
+
 function chatCommands(target, context, command, client) {
     const API = require('./callAPI.js');
     const roll = require('./rollRow.js');
+    const onlineStatus = require('./checkOnline.js');
     const whammo = require('../lists/whammoBoard.js');
-    const asciiArt = require('../lists/asciiArt.js')
+    const asciiArt = require('../lists/asciiArt.js');
 
     if (command === "!checkAPI") {
-        const cleanTarget = target.slice(1)
-        const calledAPI = API.callAPI("https://api.twitch.tv/helix/search/channels?query=" + cleanTarget)
-        .then(calledAPI => {
-            const stringAPI = JSON.stringify(calledAPI.data[0])
-            console.log(stringAPI)
-        })
-    }
-
-    if (command === "!checkOnline") {
-        const cleanTarget = target.slice(1)
-        const calledAPI = API.callAPI("https://api.twitch.tv/helix/search/channels?query=" + cleanTarget)
-        .then(calledAPI => {
-            const checkIfOnline = JSON.stringify(calledAPI.data[0].is_live)
-            console.log(checkIfOnline)
-            if(checkIfOnline == "false") {
-                console.log(cleanTarget + " is offline")
-                client.say(target, cleanTarget + " is offline")
-            }
-            if(checkIfOnline == "true") {
-                console.log(cleanTarget + " is online")
-                client.say(target, cleanTarget + " is online")
-            }
-        })
+        const calledAPI = API.callAPI("https://api.twitch.tv/helix/search/channels?query=" + target)
+            .then(calledAPI => {
+                const stringAPI = JSON.stringify(calledAPI.data[0])
+                console.log(stringAPI)
+            })
     }
 
     if (command === "!whammoRoll") {
@@ -43,14 +28,42 @@ function chatCommands(target, context, command, client) {
         client.say(target, asciiArt.toucan.unicode)
     }
 
+    if (command === "!shydog") {
+        client.say(target, asciiArt.shydog.unicode)
+    }
+
     if (command === "!test") {
         client.say(target, "I'm here! FrankerZ")
         console.log("posted in " + target + "'s chat")
     }
 
-    if(command === "BIGFROG") {
+    if (command === "!checkClient") {
+        console.log(client)
+    }
+
+    if (command === "!checkContext") {
+        console.log(context)
+    }
+
+    if (command === "BIGFROG") {
         client.say(target, "BIGFROG")
     }
+
+    if (command === "!checkOnline") {
+        // Waits to check for online status before continuing
+        // Commands needing to check online status should be wrapped in this
+        (async () => {
+            const online = await onlineStatus.checkOnline(target)
+            console.log(online)
+            if (online) {
+                console.log(target + " is online")
+            }
+            else {
+                console.log(target + " is offline")
+            }
+        })();
+    }
+
 }
 
 module.exports = { chatCommands };
